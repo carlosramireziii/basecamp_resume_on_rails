a9d3aa5d75e5d5716b054c86dc58886bf5f1048a
Carlos Ramirez III <carlos.e.ramirez.iii@gmail.com>
2020-08-18 18:08:59 -0400

    Allow todo lists to be created, show, updated, and destroyed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects do
     resources :messages
+    resources :todo_lists
     resources :uploaded_files, except: [:edit, :update]
   end
   resource :sessions, only: [:new, :create, :destroy]
