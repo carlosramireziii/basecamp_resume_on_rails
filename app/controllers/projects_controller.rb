643fba072ca437fcf48803ec7cbe84a58bba6af1
Carlos Ramirez III <carlos.e.ramirez.iii@gmail.com>
2020-08-18 03:29:39 -0400

    Implement project list and new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,25 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to [:projects], notice: 'Enjoy your new project!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,9 +1,42 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @project = projects(:my_project)
+    sign_in users(:carlos)
+  end
+
   test 'should get index' do
     get projects_path
 
     assert_response :success
+    assert_select 'h1', text: 'Projects'
+    assert_select 'h2', text: @project.title
+  end
+
+  test 'should get index with no projects' do
+    Project.delete_all
+
+    get projects_path
+
+    assert_response :success
+    assert_select 'a', text: 'Create your first project'
+  end
+
+  test 'should get new' do
+    get new_project_path
+
+    assert_response :success
+    assert_select 'h1', text: 'Create new project'
+    assert_select 'form'
+  end
+
+  test 'should post create' do
+    assert_changes 'Project.count' do
+      post projects_path, params: { project: { title: 'New project', description: 'Testing create' } }
+    end
+
+    assert_response :redirect
+    assert_redirected_to projects_path
   end
 end
