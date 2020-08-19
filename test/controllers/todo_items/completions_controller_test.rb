0d538fcff6b3c603f69ba2b5ad359aabb4b90d40
Carlos Ramirez III <carlos.e.ramirez.iii@gmail.com>
2020-08-18 20:27:31 -0400

    Add controller test for todo item completions

--- test/controllers/todo_items/completions_controller_test.rb
+++ test/controllers/todo_items/completions_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TodoItems::CompletionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @todo_item = todo_items(:my_todo_item)
+  end
+
+  test 'should post create' do
+    @todo_item.uncomplete!
+
+    post project_todo_list_todo_item_completions_path(@todo_item.project, @todo_item.todo_list, @todo_item)
+
+    assert @todo_item.reload.complete?
+  end
+
+  test 'should delete destroy' do
+    @todo_item.complete!
+
+    delete project_todo_list_todo_item_completions_path(@todo_item.project, @todo_item.todo_list, @todo_item)
+
+    refute @todo_item.reload.complete?
+  end
 end
